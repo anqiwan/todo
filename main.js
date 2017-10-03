84665914e2357a6d2d0d297c93cf90114017c11a
anqiwan <anqiwan@gmail.com>
2017-10-02 20:26:54 -0400

    exercise from class

--- main.js
+++ main.js
@@ -3,5 +3,17 @@ function addNewList() {
 }
 
 function addListItem() {
+	var list = document.getElementById("grocery-list");
+	var itemInput = document.getElementById("new-list-item");
+	var newItem = document.createElement("li");
+
+	newItem.appendChild(document.createTextNode(itemInput.value));
+	list.appendChild(newItem);
+
     console.log('hello world');
+}
+
+function deleteListItem(item) {
+	    // remove li element (item) from ol element (item.parentNode)
+ 	    item.parentNode.removeChild(item);
 }
